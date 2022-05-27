1e57de7743637093b9b76bad74a7763c1d56f920
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-05-27 01:08:21 -0500

    finished mongodb stuff

--- api/routers/categories.py
+++ api/routers/categories.py
@@ -4,11 +4,13 @@ import psycopg
 import pymongo
 import os
 import bson
+from typing import Union
+
 
 dbhost = os.environ['MONGOHOST']
 dbname = os.environ['MONGODATABASE']
 dbuser = os.environ['MONGOUSER']
-dbpass - os.environ['MONGOPASSWORD']
+dbpass = os.environ['MONGOPASSWORD']
 
 mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
 
@@ -47,51 +49,63 @@ def categories_list(page: int = 0):
     # Uses the environment variables to connect
     # In development, see the docker-compose.yml file for
     #   the PG settings in the "environment" section
-    with psycopg.connect() as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                SELECT cats.id, cats.title, cats.canon, count(*) AS num_clues
-                FROM categories AS cats
-                LEFT OUTER JOIN clues on (clues.category_id = cats.id)
-                group by cats.id, cats.title, cats.canon
-                ORDER BY cats.title
-                LIMIT 100 OFFSET %s
-            """,
-                [page * 100],
-            )
+    #with psycopg.connect() as conn:
+        # with conn.cursor() as cur:
+        #     cur.execute(
+        #         """
+        #         SELECT cats.id, cats.title, cats.canon, count(*) AS num_clues
+        #         FROM categories AS cats
+        #         LEFT OUTER JOIN clues on (clues.category_id = cats.id)
+        #         group by cats.id, cats.title, cats.canon
+        #         ORDER BY cats.title
+        #         LIMIT 100 OFFSET %s
+        #     """,
+        #         [page * 100],
+        #     )
 
-            results = []
-            for row in cur.fetchall():
-                record = {}
-                for i, column in enumerate(cur.description):
-                    record[column.name] = row[i]
-                results.append(record)
+        #     results = []
+        #     for row in cur.fetchall():
+        #         record = {}
+        #         for i, column in enumerate(cur.description):
+        #             record[column.name] = row[i]
+        #         results.append(record)
 
-            cur.execute(
-                """
-                SELECT COUNT(*) FROM categories;
-            """
-            )
-            raw_count = cur.fetchone()[0]
-            print(raw_count)
-            page_count = (raw_count // 100) + 1
+        #     cur.execute(
+        #         """
+        #         SELECT COUNT(*) FROM categories;
+        #     """
+        #     )
+        #     raw_count = cur.fetchone()[0]
+        #     print(raw_count)
+        #     page_count = (raw_count // 100) + 1
 
-            return Categories(page_count=page_count, categories=results)
+        #     return Categories(page_count=page_count, categories=results)
     ##___________________________________________
+    #creates a new Mongo Client to talk to the DDBMS
+    client = pymongo.MongoClient(mongo_str)
+    #Gets the database from the value of an environment variable
+    db = client[dbname]
+    #Finds categories sorted by title, skipping the categories not needed due to the
+    #page parameter, and limiting the results to 100
+    categories = db.categories.find().sort("title").skip(100*page).limit(100)
+    #turn the object returned from the query into a list
+    categories = list(categories)
+    #Loop over each category in the list
+    for category in categories:
+        #counts the number of clues with the specified category_id
+        count = db.command({
+            "count": "clues",
+            "query": { "category_id": category["_id"] }
+        })
+        category["num_clues"] = count["n"]
+        category["id"] = category["_id"]
+        del category["_id"]
+    page_count = db.command({"count": "categories"})["n"] // 100
+    return {
+        "page_count": page_count,
+        "categories": categories,
+    }
 
-    # client = pymongo.Mongo(mongo_str)
-    # db = client[dbname]
-    # print(db)   #print to check that there is a connection to mongodb
-    # categories = db.categories.find().sort('title', pymongo.ASCENDING).skip(page*100).limit(100)
-    #db.categories
-    
-
-    ## call it in insomnia with id# you want
-
-    #print(category) #to check
-    
-    ## look at localhost:8081/db/trivia-game/ to see mongo stuff
             
 
 #getdetail
@@ -101,35 +115,32 @@ def categories_list(page: int = 0):
     responses={404: {"model": Message}},
 )
 def get_category(category_id: int, response: Response):
-    with psycopg.connect() as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                f"""
-                SELECT id, title, canon
-                FROM categories
-                WHERE id = %s
-            """,
-                [category_id],
-            )
-            row = cur.fetchone()
-            if row is None:
-                response.status_code = status.HTTP_404_NOT_FOUND
-                return {"message": "Category not found"}
-            record = {}
-            for i, column in enumerate(cur.description):
-                record[column.name] = row[i]
-            return record
-
+    # with psycopg.connect() as conn:
+    #     with conn.cursor() as cur:
+    #         cur.execute(
+    #             f"""
+    #             SELECT id, title, canon
+    #             FROM categories
+    #             WHERE id = %s
+    #         """,
+    #             [category_id],
+    #         )
+    #         row = cur.fetchone()
+    #         if row is None:
+    #             response.status_code = status.HTTP_404_NOT_FOUND
+    #             return {"message": "Category not found"}
+    #         record = {}
+    #         for i, column in enumerate(cur.description):
+    #             record[column.name] = row[i]
+    #         return record
 # __________________________________________
+    client = pymongo.MongoClient(mongo_str)
+    db = client[dbname]
+    result = db.categories.find_one({"_id": category_id})
+    result["id"] = result["_id"]
+    del result["_id"]
+    return result
 
-    # client = pymongo.Mongo(mongo_str)
-    # db = client[dbname]
-    # print(db)   #print to check that there is a connection to mongodb
-    # category = db.categories.find_one({ "_id": category_id })
-    ## call it in insomnia with id# you want
-    #print(category) #to check
-    #return category
-    ## look at localhost:8081/db/trivia-game/ to see mongo stuff
 
 
 @router.post(
